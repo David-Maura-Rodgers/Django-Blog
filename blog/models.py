6e51c6a09e885d709549222ac1c7846ade4210ff
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-07-11 22:17:35 +0000

    can add and approve comments

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@
 # pip3 install 'django<4' gunicorn
 # pip3 install django-allauth
 # pip3 install django-summernote
+# pip3 install django-crispy-forms
 # pip3 freeze -- local > requirements.txt
 
 # ls ../.pip-modules/lib
@@ -18,26 +19,27 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, 'Draft')), ((1, 'Published'))
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now=True)
-    context = models.TextField()
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    context = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
         User, related_name='blogpost_like', blank=True)
 
-
-class Meta:
-    ordering = ["-created_on"]
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
         return self.title
@@ -47,8 +49,8 @@ class Meta:
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
